80f645b575d9f5ce55e19a19a666b3ff3f5676a7
Sergei Mishanin <mishaninsi@yandex.ru>
2022-06-18 14:14:24 +0300

    правки второе ревью

--- script/index.js
+++ script/index.js
@@ -1,12 +1,18 @@
 const profileCollumnButton = document.querySelector('.profile__column-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-btn');
-
 profileCollumnButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
+let profileName = document.querySelector('.profile__column-name');
+let profileProfession = document.querySelector('.profile__column-profession');
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__input_name');
+let jobInput = formElement.querySelector('.popup__input_job');
 
 
 function openPopup(event) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileProfession.textContent;
     popup.classList.add('popup_opened')
     }
 
@@ -14,11 +20,7 @@ function closePopup(event) {
     popup.classList.remove('popup_opened')
         }
 
-let profileName = document.querySelector('.profile__column-name');
-let profileProfession = document.querySelector('.profile__column-profession');
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.name-input');
-let jobInput = formElement.querySelector('.job-input');
+
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
