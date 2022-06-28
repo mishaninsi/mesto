529c9ffca59cf3552bea1b7d1bfb3208504e71f8
Sergei Mishanin <mishaninsi@yandex.ru>
2022-06-28 08:38:57 +0300

    popup new place

--- script/index.js
+++ script/index.js
@@ -1,8 +1,20 @@
 const profileCollumnButton = document.querySelector('.profile__column-button');
+
+const profileAddbutton = document.querySelector('.profile__addbutton');
+
 const popup = document.querySelector('.popup');
+
+const popupplace = document.querySelector('.popup_place');
+
 const popupCloseButton = document.querySelector('.popup__close-btn');
+const placeCloseButton = document.querySelector('.popup__close-btn_place');
+
 profileCollumnButton.addEventListener('click', openPopup);
+profileAddbutton.addEventListener('click', openPopupPlace);
+
+
 popupCloseButton.addEventListener('click', closePopup);
+placeCloseButton.addEventListener('click', closePopupPlace);
 let profileName = document.querySelector('.profile__column-name');
 let profileProfession = document.querySelector('.profile__column-profession');
 let formElement = document.querySelector('.popup__container');
@@ -16,10 +28,17 @@ function openPopup(event) {
     popup.classList.add('popup_opened')
     }
 
+function openPopupPlace(event) {
+    popupplace.classList.add('popup_opened')
+    }
+
 function closePopup(event) {
     popup.classList.remove('popup_opened')
         }
 
+function closePopupPlace(event) {
+    popupplace.classList.remove('popup_opened')
+                }
 
 
 function formSubmitHandler(evt) {
@@ -30,3 +49,41 @@ function formSubmitHandler(evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler)
+
+const cardsTemplate = document.querySelector('#Cards').content;
+const elements = document.querySelector('.elements');
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+  initialCards.forEach(function(newcard){
+  const card = cardsTemplate.cloneNode(true);
+  console.log(newcard.title);
+  card.querySelector('.element__place-name').textContent = newcard.name;
+  card.querySelector('.element__photo').src = newcard.link;
+  elements.append(card);
+  });
