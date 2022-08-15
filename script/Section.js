67746fa3c68ac8bd9dc6c2698a400c10fd5ae754
Sergei Mishanin <mishaninsi@yandex.ru>
2022-08-15 15:27:16 +0300

    Section class

---
+++ script/Section.js
@@ -0,0 +1,7 @@
+export class Section {
+    constructor({ items, renderer }, containerSelector) {
+        this._renderedItems = items;
+        this._renderer = renderer;
+        this._container = containerSelector;
+    }
+}
