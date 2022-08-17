c4a3ea2d9e65482e42435f761e9dbf45c16b0673
Sergei Mishanin <mishaninsi@yandex.ru>
2022-08-17 13:01:35 +0300

    Popup+PopupWithImage classes

---
+++ script/Popup.js
@@ -0,0 +1,30 @@
+export default class Popup {
+    constructor (popupSelector) {
+        this._popupSelector = popupSelector;
+        this._closeButtons = this._popupSelector.querySelector('.popup__close-btn');
+    }
+
+    open() {
+        this._popupSelector.classList.add('.popup_opened');
+        this._handleEscClose();
+    }
+
+    close() {
+        this._popupSelector.classList.remove('.popup_opened');
+        this._handleEscClose();
+    }
+
+    _handleEscClose (evt) {
+        evt.preventDefault();
+        if (evt.key === 'Escape') {
+            const popupActive = document.querySelector('.popup_opened');
+            this.close(popupActive);
+        }
+    }
+
+    setEventListeners() {
+        this._closeButtons.ddEventListener('click', () => {
+            this.close();
+        })
+    }
+}
---
+++ script/PopupWithImage.js
@@ -0,0 +1,16 @@
+import Popup from "./Popup";
+
+export default class PopupWithImage extends Popup{
+constructor (popupSelector) {
+super(popupSelector);
+this._popupImage = this.popupSelector.querySelector('.popup__image');
+this._popupPlaceName = this.popupSelector.querySelector('.popup__place-name');
+}
+
+open(link,name) {
+    this._popupImage.src = link;
+    this._popupPlaceName.textcontent = name;
+    this._popupImage.alt = name;
+    super.open();
+    }
+}
